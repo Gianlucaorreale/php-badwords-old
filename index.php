b38a3f7984037e34707ad93eea11b898bfbef76b
Gianlucaorreale <103740550+Gianlucaorreale@users.noreply.github.com>
2022-09-08 17:46:02 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
- $string = 'Ciao sono Gianluca e sto iniziando ad usare ';
- var_dump($string);
- $name = $_GET["name"];
- $full_string = "$string $name";
- var_dump($full_string);
-?>
+ $paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis a quis quisquam dolor vero, eius,
+  ipsa quaerat nulla iusto tenetur blanditiis nihil distinctio pariatur sunt hic consectetur aliquid aspernatur!';
+
+ $badword = 'a';
+
+ $censured_paragraph = str_replace($badword, '***', $paragraph);
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+   <p><?php echo $paragraph ?></p>
+   <p><em>il paragrafo è lungo <?php echo strlen($paragraph)?> caratteri</em></p> 
+   <hr>
+   <p><?php echo $censured_paragraph ?></p>
+   <p><em>il paragrafo è lungo <?php echo strlen($censured_paragraph)?> caratteri</em></p> 
+ </body>
+ </html>
